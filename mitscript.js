d79e3da937e39da1584f04fb1246e01644ff1dbc
majajohannsen <johannsen12@hotmail.com>
2020-03-16 16:46:50 +0100

    version 7

--- mitscript.js
+++ mitscript.js
@@ -1,3 +1,15 @@
+"use strict";
+
 function mytoggle() {
     document.getElementById("sidebar").classList.toggle('active');
 }
+fetch("https://dawa.aws.dk/postnumre")
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (JSON) {
+        console.log(JSON[0])
+        for (let i = 0; i < JSON.length; i++) {
+            document.getElementById("pbliste").insertAdjacentHTML('afterbegin', "<option>" + JSON[i].nr + " " + JSON[i].navn + "<option>");
+        }
+    })
