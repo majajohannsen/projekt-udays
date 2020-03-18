e8c1e8e5f2400a000d6d259942ae730a178a60b0
majajohannsen <johannsen12@hotmail.com>
2020-03-18 17:07:28 +0100

    version 31

--- mitscript.js
+++ mitscript.js
@@ -8,8 +8,13 @@ fetch("https://dawa.aws.dk/postnumre")
         return data.json();
     })
     .then(function (JSON) {
-        console.log(JSON[0])
-        for (let i = 0; i < JSON.length; i++) {
-            document.getElementById("pbliste").insertAdjacentHTML('afterbegin', "<option>" + JSON[i].nr + " " + JSON[i].navn + "<option>");
-        }
-    })
+            console.log(JSON[0])
+            for (let i = 0; i < JSON.length; i++) {
+                document.getElementById("pbliste").insertAdjacentHTML('afterbegin', "<option>" + JSON[i].nr + " " + JSON[i].navn + "<option>");
+            }
+
+            const lydobjektet = new Audio("audioclip.mp4");
+
+            document.getElementById("playBut").addEventListener("click", function () {
+                lydobjektet.play();
+            });
